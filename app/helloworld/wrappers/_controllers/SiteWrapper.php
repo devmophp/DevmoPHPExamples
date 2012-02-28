e5d377f87a1eed3ceea03cd2f15fb04c0f57d283
dan wager <dan_wager@medigap360.com>
2012-02-28 16:18:15 -0600

    updated view::get/setToken() to view::get/set()

--- app/helloworld/wrappers/_controllers/SiteWrapper.php
+++ app/helloworld/wrappers/_controllers/SiteWrapper.php
@@ -4,9 +4,9 @@ namespace helloworld\wrappers\controllers;
 abstract class SiteWrapper extends \devmo\controllers\Controller {
 	public function addWrapper ($content) {
 		$view = $this->getView('helloworld.wrappers.views.SiteWrapper');
-		$view->setToken('navigation',$this->runController('helloworld.wrappers.Navigation'));
-		$view->setToken('content',$content);
-		$view->setToken('poweredby',$this->runController('devmo.PoweredBy'));
+		$view->set('navigation',$this->runController('helloworld.wrappers.Navigation'));
+		$view->set('content',$content);
+		$view->set('poweredby',$this->runController('devmo.PoweredBy'));
 		return $view;
 	}
 }
